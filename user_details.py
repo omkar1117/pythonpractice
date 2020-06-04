4f873ee3b8895f84db49fbd4ad410306fb4bc02e
Omkar P <git.omme@gmail.com>
2020-06-04 16:29:06 +0530

    User details program updated

--- user_details.py
+++ user_details.py
@@ -1,10 +1,24 @@
-name = input("Enter your Full Name:")
+name = input("Enter your Full Name : (FirstName-LastName):")
 mobile = input("Enter your phone number:")
 address = input("Ener your Address:")
 
-print("----------------- User Details ----------------")
+print("----------------- User Enter Details ----------------")
 print("Your entered values are")
 print("User Full Name is:", name)
 print("User Mobile no. is:", mobile)
 print("User Address is:", address)
 print("----------------- The End ----------------")
+
+data = name.split("-")
+# print("Splitted Data:", data)
+
+if len(data) < 2:
+    print("Invalid Name: Enter your Full Name in this format : (FirstName-LastName)")
+
+if len(mobile) != 10:
+    print("Invalid mobile number")
+else:
+    mobile = int(mobile)
+
+if not address:
+    print("Enter a valid Address") 
